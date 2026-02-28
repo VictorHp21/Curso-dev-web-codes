f9fde1207943e0edc13d2f33e9a25ccf711431e6
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2026-02-28 17:35:18 -0300

    criando posições randômicas

--- secao10_javascript/AppGame_mata_mosquito/estilo/imagens/jogo.js
+++ secao10_javascript/AppGame_mata_mosquito/estilo/imagens/jogo.js
@@ -7,12 +7,46 @@ var altura
 var largura
 
 function ajustaTamPalcoJogo() {
-     altura = window.innerHeight
-     largura = window.innerWidth
+    altura = window.innerHeight
+    largura = window.innerWidth
 
-     console.log(altura, largura)
+    console.log(largura, altura)
 }
 
 
-ajustaTamPalcoJogo()
+ajustaTamPalcoJogo() // tem que chamar a função antes para ter os valores de altura e largura
+
+// criar posições randômicas
+
+// Math.floor == arredondamento para baixo
+
+
+
+
+function posicaoRandomica() {
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    // criar o elemento html
+
+    var mosquito = document.createElement('img')
+
+    mosquito.src = '../mosca.png'
+    mosquito.className = 'mosquito1'
+
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
+
+
+
 
