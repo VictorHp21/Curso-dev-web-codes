59479824ecf9acae2bbf67928506ea035eb257f1
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2026-02-28 17:10:10 -0300

    criando variáveis randomicas com o tamalho palco do jogo

---
+++ secao10_javascript/AppGame_mata_mosquito/estilo/imagens/jogo.js
@@ -0,0 +1,18 @@
+
+
+// é necessário q estejam atualizadas sempre que a tela aumente ou diminuia
+
+
+var altura
+var largura
+
+function ajustaTamPalcoJogo() {
+     altura = window.innerHeight
+     largura = window.innerWidth
+
+     console.log(altura, largura)
+}
+
+
+ajustaTamPalcoJogo()
+
